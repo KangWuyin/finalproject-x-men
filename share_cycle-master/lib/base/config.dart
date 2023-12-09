e3e40d449c617738ff053561cd4a5d7c5322af16
KangWuyin <144210677+KangWuyin@users.noreply.github.com>
2023-12-08 22:33:59 -0800

    Delete share_cycle-master directory

--- share_cycle-master/lib/base/config.dart
+++
@@ -1,3 +0,0 @@
-
-String baseBucket = "gs://sharecycle-6b853.appspot.com";
-String baseDownloadBucket = "sharecycle-6b853.appspot.com";
--- share_cycle-master/lib/utils/snack_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:oktoast/oktoast.dart';
-
-class SnackUtils {
-  static showSnack(BuildContext context, String msg) {
-    showToast(msg,position: ToastPosition.bottom);
-  }
-}
